44cfd798197fc469062803188edc0921a9a3182a
Min Phone Aung <88699874+PeterHyperAung@users.noreply.github.com>
2023-07-21 01:21:50 +0630

    second commit

--- index.js
+++ index.js
@@ -10,26 +10,12 @@ const fs = require("fs");
     await page.goto("https://bold.org/blog/college/");
     obj.title = await page.evaluate(() => document.title);
 
-    //   const posts = await page.$$eval("[href]", (el) => {
-    //     console.log("HELLO", el);
-    //     return el.innerText;
-    // return {
-    //   title: el.lastChild.children[1].innerText,
-    //   description: el.lastChild.children[2].innerText,
-    //   date: el.lastChild.firstChild.firstChild.firstChild.innerText,
-    //   imageLink: el.firstChild.style.backgroundImage.slice(5, -2),
-    // };
-    //   });
-
     obj.posts = await page.evaluate(() =>
       Array.from(
         document.querySelectorAll(
           ".PostsGrid-module--container--5ac84 a[href^='/blog']"
         ),
         (el) => {
-          //   const imageLink = await document.querySelector(
-          //     `a[href="${el.href}"] div.PostsGrid-module--image--28378`
-          //   ).styles;
           const [date, duration, title, description] = el.innerText.split("\n");
           return {
             title,
